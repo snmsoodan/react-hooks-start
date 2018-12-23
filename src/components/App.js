6bb58df0b66954e1c1c845a447b4b12f6489fc39
sanamdeep singh <sanamsoodan@gmail.com>
2018-12-22 20:53:14 -0500

    working react-hooks without code reuse

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import ResourceList from "./ResourceList";
+
+const App = () => {
+
+    const [resource, setResource] = useState('posts')
+    
+        return(
+            <div>
+                <div>
+                    <button onClick={() => setResource('posts') }>Post</button>
+                    <button onClick={() => setResource('todos') }>To Do</button>
+                </div>
+                <ResourceList resource={resource} />
+            </div>
+        )
+}
+
+export default App;
