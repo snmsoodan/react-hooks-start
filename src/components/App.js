40ac327e77af5288af9da0fc005f4957a8ff748a
sanamdeep singh <sanamsoodan@gmail.com>
2018-12-22 21:09:36 -0500

    added users component for reusing hooks

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import ResourceList from "./ResourceList";
+import UserList from './UserList';
 
 const App = () => {
 
@@ -7,6 +8,7 @@ const App = () => {
     
         return(
             <div>
+                <UserList  />
                 <div>
                     <button onClick={() => setResource('posts') }>Post</button>
                     <button onClick={() => setResource('todos') }>To Do</button>
--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,23 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import axios from "axios";
-
+import React from 'react';
+import useResources from "./useResources";
 
 const ResourceList = ({resource}) => {
 
-    const [resources, setResources] = useState([])
-
-    const fetchResource = async () => {
-        const response = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`)
-        setResources(response.data)
-    }
-
-    useEffect(() => {
-        fetchResource(resource)
-    },[resource])
-
+    const resources = useResources(resource);
         return (
             <ul>{resources.map(record => (
-                <li key='record.id'>{record.title}</li>
+                <li key={record.id}>{record.title}</li>
             ))}</ul>
         )
 }
